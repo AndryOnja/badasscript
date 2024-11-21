e85aa7462bf9f1c1f67bc5933aff3907eb54d863
onja <kblandryonja@gmail.com>
2024-11-21 08:50:15 +0300

    CRUD user avec validation request

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Dashboard\UserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +34,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::name("dashboard.")->prefix("/dashboard")->middleware(['auth', 'verified'])->group(function() {
+    Route::resource('/users', UserController::class);
 });
 
 require __DIR__.'/auth.php';
